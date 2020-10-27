514e39b3916807ac114b5603f11f0a8ca92a0e38
Haruk <haruka.lex@gmail.com>
2020-10-27 12:54:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const monsterZone = document.getElementById('zone');
-const zoneListButton = document.querySelector('.tab-zone');
 const monsterZoneList = document.getElementById('monster');
 const monsterData = document.getElementById('data');
+const zoneListButton = document.querySelectorAll('.tab-zone');
+const zoneMonsterButton = document.querySelectorAll('.tab-monster');
 
-zoneListButton.addEventListener('click', activeButton);
-
-function activeButton (e) {
-const buttonClick = e.target;
-    zoneListButton.classList.remove('btn-primary');
-    zoneListButton.classList.add('active-primary');
-
+function clickButton() {
+    zoneListButton.addEventListener('click', activeButton);
 };
+
+zoneListButton.forEach(function addClass(item){
+    item.classList.add('active-primary');
+});
+
+// zoneListButton.forEach(function addClass(item){
+//     item.addClass('active-primary');
+// }); 
