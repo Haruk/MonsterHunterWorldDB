11b177a8599b5fff238360dc092029b9e0f878e0
Haruk <haruka.lex@gmail.com>
2020-11-02 12:12:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,62 @@ const monsterData = document.getElementById('data');
 const zoneListButton = document.querySelectorAll('.tab-zone');
 const zoneMonsterButton = document.querySelectorAll('.tab-monster');
 
-function clickButton() {
+
+zoneListButton.forEach(function(zoneListButton) {
     zoneListButton.addEventListener('click', activeButton);
-};
+    });
 
-zoneListButton.forEach(function addClass(item){
-    item.classList.add('active-primary');
-});
+function activeButton (e) {
+    const buttonClick = e.target;
+        if (buttonClick.classList.contains('btn-primary')){
+            buttonClick.classList.replace('btn-primary','active-primary');
+        }else {
+            buttonClick.classList.replace('active-primary','btn-primary');
+        };
+    };
 
-// zoneListButton.forEach(function addClass(item){
-//     item.addClass('active-primary');
-// }); 
+// Zmieania klasę na active po kliknięciu. DZIAŁA (zostaje klasa active)
+    // function activeButton (e) {
+    //     const buttonClick = e.target;
+    //     buttonClick.classList.remove('btn-primary');
+    //     buttonClick.classList.add('active-primary'); 
+    // };
+
+//Zmienia klasy po kliknięciu na active, a po kolejnym z powrotem na btn-primary. DZIAŁA
+// function activeButton (e) {
+//     const buttonClick = e.target;
+//             if (buttonClick.classList.contains('btn-primary')){
+//             buttonClick.classList.remove('btn-primary');
+//             buttonClick.classList.add('active-primary'); 
+//         }else {
+//             buttonClick.classList.remove('active-primary');
+//             buttonClick.classList.add('btn-primary');
+//         }
+//     };
+
+//Zmienia klasy po kliknięciu na active, a po kolejnym z powrotem na btn-primary. WERSJA 2 (DZIAŁA!)
+
+// function activeButton (e) {
+//     const buttonClick = e.target;
+    
+//         if (buttonClick.classList.contains('btn-primary')){
+//             buttonClick.classList.replace('btn-primary','active-primary');
+//         }else {
+//             buttonClick.classList.replace('active-primary','btn-primary');
+//         };
+//     };
+
+
+// Sprawdzenie czy klasa active-button jest juz wcześniej nadana i ją wyłącza, za to nadaje ją klikniętemu buttonowi IN PROGRESS
+// function activeButton (e) {
+//     const buttonClick = e.target;
+//     for (let i=0; i<zoneListButton.length; i++){
+//         if (buttonClick.classList.contains('btn-primary')){
+//             buttonClick.classList.remove('btn-primary');
+//             buttonClick.classList.add('active-primary'); 
+//         }else {
+//             buttonClick.classList.remove('active-primary');
+//             buttonClick.classList.add('btn-primary');
+//         }
+//     }};
+
