08661f6121e1361eea08f602aa2a5f3cb13aabf7
Haruk <haruka.lex@gmail.com>
2020-11-06 15:52:11 +0100

    Updated functions, clean up code

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const monsterData = document.getElementById('data');
+const monsterRow = document.getElementById('monster');
 const zoneList = document.querySelectorAll('.tab-zone');
 
+const monsterList = {
+   'Ancient Forest': ['Anjaneth','Great Jagras','PukeiPukei','Tobi Kadachi','Kulu JaKu','Rathalos'],
+   'Wilspire Waste' : ['Baroth','Juratodus','Diablos','Black Diablos','Rathian'],
+   'Coral Highlands' : ['Legiana','Paloumu','TziTzi JaKu','Pink Rathian'],
+   'Rotten Valley' : ['Radoban','Great Girros','Odogaron'],
+   'Elder Recess' : ['Uragan', 'Lavasioth','Dodogama','Azure Rathalos'],
+   'Elder Dragons' : ['Kirin','Nergigante','Kushala Daora','Teostra','Lunastra','Val Hazaak', 'Xenojiva','Kulve Taroth'],
+   'Invading Monsters' : ['Devil Jho','Bazelgeuse']
+};
+
+const ancientMonsterList = ['Anjaneth','Great Jagras','PukeiPukei','Tobi Kadachi','Kulu JaKu','Rathalos'];
+const wildspireMonsterList = ['Baroth','Juratodus','Diablos','Black Diablos','Rathian'];
+const coralMonsterList = ['Legiana','Paloumu','TziTzi JaKu','Pink Rathian'];
+const rottenMonsterList = ['Radoban','Great Girros','Odogaron'];
+const elderMonsterList = ['Uragan', 'Lavasioth','Dodogama','Azure Rathalos'];
+const invadingMonsterList = ['Devil Jho','Bazelgeuse'];
+const dragonMonsterList = ['Kirin','Nergigante','Kushala Daora','Teostra','Lunastra','Val Hazaak', 'Xenojiva','Kulve Taroth'];
  
 
 zoneList.forEach(function(zoneButton) {
@@ -25,28 +43,23 @@ function checkIfActive() {
 
 // Wyświetla liste potworów z danego zone'a
 
-function createMonsterButton() {
+function createMonsterButton () {
+    for (i=0; i<ancientMonsterList.length; i++){
     const monsterButton = document.createElement('button');
     monsterButton.className = 'btn-secondary tab-monster';
     monsterRow.appendChild(monsterButton);
-    for(i=0; i<ancientMonsterList.length; i++){
-        monsterButton.innerHTML=ancientMonsterList[i];
+    monsterButton.innerHTML = ancientMonsterList[i];
     };
 };
 
 
 
-
-//Zmienia klasy po kliknięciu na active, a po kolejnym z powrotem na btn-primary. WERSJA 2 (DZIAŁA!)
-
-// function activeButton (e) {
-//     const buttonClick = e.target;
-//         if (buttonClick.classList.contains('btn-primary')){
-//             buttonClick.classList.replace('btn-primary','active-primary');
-//         }else {
-//             buttonClick.classList.replace('active-primary','btn-primary');
-//         };
-//     };
+// Zmieania klasę na active po kliknięciu. DZIAŁA (zostaje klasa active)
+    // function activeButton (e) {
+    //     const buttonClick = e.target;
+    //     buttonClick.classList.remove('btn-primary');
+    //     buttonClick.classList.add('active-primary'); 
+    // };
 
 //Zmienia klasy po kliknięciu na active, a po kolejnym z powrotem na btn-primary. DZIAŁA
 // function activeButton (e) {
@@ -60,10 +73,25 @@ function createMonsterButton() {
 //         }
 //     };
 
-// Zmieania klasę na active po kliknięciu. DZIAŁA (zostaje klasa active)
-    // function activeButton (e) {
-    //     const buttonClick = e.target;
-    //     buttonClick.classList.remove('btn-primary');
-    //     buttonClick.classList.add('active-primary'); 
-    // };
+//Zmienia klasy po kliknięciu na active, a po kolejnym z powrotem na btn-primary. WERSJA 2 (DZIAŁA!)
+
+// function activeButton (e) {
+//     const buttonClick = e.target;
+//         if (buttonClick.classList.contains('btn-primary')){
+//             buttonClick.classList.replace('btn-primary','active-primary');
+//         }else {
+//             buttonClick.classList.replace('active-primary','btn-primary');
+//         };
+//     };
+
+// Wyświetla liste potworów z danego zone'a wersja robocza DZIAŁA
+
+// function createMonsterButton() {
+//     const monsterButton = document.createElement('button');
+//     monsterButton.className = 'btn-secondary tab-monster';
+//     monsterRow.appendChild(monsterButton);
+//     for(i=0; i<ancientMonsterList.length; i++){
+//         monsterButton.innerHTML=ancientMonsterList[i];
+//     };
+// };
 
